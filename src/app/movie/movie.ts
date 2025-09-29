6529bfcb34913f0becf54bd49d13ba06b66d9f11
Sneakyweasel90 <neil.mannion1@gmail.com>
2025-09-29 09:40:36 -0300

    update movies

--- src/app/movie/movie.ts
+++ src/app/movie/movie.ts
@@ -34,13 +34,13 @@ export class Movie {
 
   setUrl() {
     if (this.type === 'trending') {
-      this.url = 'http://localhost:4200/assets/data/trending-movies.json';
+      this.url = 'assets/data/trending-movies.json';
     }
     if (this.type === 'theatre') {
-      this.url = 'http://localhost:4200/assets/data/theatre-movies.json';
+      this.url = 'assets/data/theatre-movies.json';
     }
     if (this.type === 'popular') {
-      this.url = 'http://localhost:4200/assets/data/popular-movies.json';
+      this.url = 'assets/data/popular-movies.json';
     }
   }
 
